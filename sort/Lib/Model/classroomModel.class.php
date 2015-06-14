e48dde258a49e1054c91fa5ca20aa72acff57064
wangtianlu <wangtianlu0709@163.com>
2015-06-14 20:20:15 +0800

    modified sortAction

--- sort/Lib/Model/classroomModel.class.php
+++ sort/Lib/Model/classroomModel.class.php
@@ -3,7 +3,6 @@
 class classroomModel extends Model{
     /**
      * ID of the classroom
-     * 紫金港西一-101: campus/equip/capacity/sequence_number
      * @var int
      */
     protected $id;
@@ -16,83 +15,141 @@ class classroomModel extends Model{
 
     /**
      * campus of the classroom
-     * 0=zjg,1=yq,2=xx,3=hjc,4=zj
      * @var string
      */
     protected $campus;
 
     /**
+     * building of the classroom e.g.教七
+     * @string
+     */
+    protected $building;
+    /**
+     * position e.g.306
+     * @int
+     */
+    protected $position;
+    /**
      * if the classroom has equipment for lab 是=1；否=0
      * @var string;
      */
-    protected $equip;
+    protected $lab;
+    /**
+     * opentime
+     * @var string
+     */
+    protected $opentime;
+    /**
+     * close time
+     * @var string
+     */
+    protected $closetime;
 
     /**
      * capacity
+     * 0:<50,1:50~100,2:100~200
      * @var int
      */
     protected $capacity;
 
     /**
-     * sequence number of this kind of classroom
-     * @var int
+     * store the information about Monday to Friday
+     * @array
      */
-    protected $sequence_number;
-
-
+    protected  $timeArray1=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);//40
+    /**
+     ** store the information about Sat.and Sun.
+     */
+    protected  $timeArray2=array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);//16
     /**
      * @var int
      * @return string name
      */
-    public function getName($id){
-        $condition['id'] = $id;
-        return $this->field('name')->where($condition)->find();
-    }
-    /**
-     * @var int
-     * @return int campus
-     */
-    public function getCampus($id){
-        $condition ['id'] = $id;
-        $campus = $this->field('campus') ->where($condition)->find();
-        if($campus == '紫金港')
-            return 0;
-        elseif($campus == '玉泉')
-            return 1;
-        elseif($campus == '西溪')
-            return 2;
-        elseif($campus == '华家池')
-            return 3;
-        elseif($campus == '之江')
-            return 4;
-    }
-    /**
-     * @var int
-     * @return int
-     */
-    public  function getEquip($id){
-        $condition['id'] = $id;
-        $equip = $this->field('equip') ->where($condition)->find();
-        if($equip == '是')
-            return 1;
-        else
-            return 0;
-    }
-
+//    public function getName($id){
+//        $condition['id'] = $id;
+//        return $this->field('name')->where($condition)->find();
+//    }
+//    /**
+//     * @var int
+//     * @return int campus
+//     */
+//    public function getCampus($id){
+//        $condition ['id'] = $id;
+//        $campus = $this->field('campus') ->where($condition)->find();
+//        if($campus == '紫金港')
+//            return 0;
+//        elseif($campus == '玉泉')
+//            return 1;
+//        elseif($campus == '西溪')
+//            return 2;
+//        elseif($campus == '华家池')
+//            return 3;
+//        else
+//            return 4;
+//    }
+//    /**
+//     * @var int
+//     * @return int
+//     */
+//    public  function getEquip($id){
+//        $condition['id'] = $id;
+//        $equip = $this->field('equip') ->where($condition)->find();
+//        if($equip == '是')
+//            return 1;
+//        else
+//            return 0;
+//    }
+//
     /**
      * @var int
      * @return int
      */
     public  function  getCapacity($id){
-        $condition['id'] = $id;
-        return $this->field('capacity')->where($condition)->find();
+        //$condition['id'] = $id;
+        //$capacity = $this->field('capacity')->where($condition)->find();
+        return $this->field('capacity')->select();
+    }
+//    /**
+//     * @var int
+//     * @return int
+//     */
+//    public function getSequenceNumber($id){
+//        $condition['id'] = $id;
+//        return $this->field('sequence_number')->where($condition)->find();
+//    }
+    /**
+     * @var $int
+     * @return array
+     */
+//    public  function getTimetArray1($id){
+//        $condition['id'] = $id;
+//        return $this->field('timeArray1')->where($condition)->find();
+//    }
+    /**
+     * syncronize
+     * @return null
+     */
+    public  function  syncronize(){
+        foreach( $this->select() as $i)
+            $capacity[$i['id']]=$i['$capacity'];
     }
     /**
-     * @var int
+     * update the timeArray1
      * @return int
      */
-    public function getSequenceNumber($id){
+    public function updateArray1($id, $newArray){
         $condition['id'] = $id;
-        return $this->field('sequence_number')->where($condition)->find();
+        $this->where($condition)->setField('timeArray1',$newArray);
+        return 0;
     }
+    /**
+     * update the timeArray2
+     * @return int
+     */
+    public function updateArray2($id, $newArray){
+        $condition['id'] = $id;
+        $this->where($condition)->setField('timeArray2',$newArray);
+        return 0;
+    }
+
 }
---
+++ sort/Lib/Model/course_classModel.class.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: WangTianlu
+ * Date: 2015/6/13
+ * Time: 19:05
+ */
